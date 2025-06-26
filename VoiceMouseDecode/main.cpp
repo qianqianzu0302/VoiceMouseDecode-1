10980b7e46f260f006bc013f52cf744cfd586622
QianqianZu <qizu@microsoft.com>
2025-06-26 09:29:35 +0800

    Initial Commit

---
+++ VoiceMouseDecode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VoiceMouseDecode
+//
+//  Created by Qianqian Zu on 2025/6/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
